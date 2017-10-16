c624aba6731adae4055ec175b5073c6297cf26d9
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-10-16 10:37:00 +0100

    Major script changes:
     - use the configure/Make files in the lalsuite base directory, rather than
       the individual ones in each seperate module
     - allow individual modules to be disabled on the command line (all are built
       by default)
     - change from using bash getopts to the GNU getopt function for passing
       command line arguments

--- virtualenv_reinstall.sh
+++ virtualenv_reinstall.sh
@@ -4,14 +4,6 @@
 
 LALSUITE_LOCATION=${HOME}/lscsoft/lalsuite
 
-# current location
-CURDIR=`pwd`
-
-# get a list of the current git branch names
-cd $LALSUITE_LOCATION
-
-branches=$(git branch | cut -c 3-) # see http://stackoverflow.com/a/3846451/1862861
-
 # command lines arguments:
 #   -b branchname (the git branch name) [required]
 #   -n (do not install additional python packages)
@@ -21,21 +13,21 @@ branches=$(git branch | cut -c 3-) # see http://stackoverflow.com/a/3846451/1862
 #   -g perform 'git clean -dxf' to remove superfluous files 
 #   -v (path to the virtualenvwrapper.sh script)
 #   -o compile with further optimisation
-#   -d compile without doxygen documentation
 #   -C perform a "make check"
 #   -h (print this info)
 
-usage="Usage $0 -b (-n -u -c -g -p -v -o -d -C -h):\n\t-b\tbranchname (the git branch name) [required]\n\
-\t-n\tdo not install a selection of additional python packages [optional]\n\
-\t-u\tuninstall previous lalsuite install (rather than just doing a 'make distclean') [optional]\n\
-\t-c\tonly re-run 'make install' without 'make distclean' beforehand [optional]\n\
-\t-g\tperform 'git clean -dxf' to remove superfluous files (BE CAREFUL!) [optional]\n\
-\t-p\tpython (the path to the required python executable for installing\n\t\tthe virtual environment) [optional]\n\
-\t-v\tpath to the virtualenvwrapper.sh script [optional]\n\
-\t-o\tcompile lalsuite with the -O3 optimisation [optional]\n\
-\t-d\tcompile lalsuite without doxygen documentation (this is enabled by default)\n\
-\t-C\trun 'make check' after installation\n\
-\t-h\thelp\n"
+usage="Usage $0 -b (-n -u -c -g -p -v -o -d -C --disable-lal[packagename] --disable-doxygen -h):\n\t-b\t\t\t\tbranchname (the git branch name) [required]\n\
+\t-n\t\t\t\tdo not install a selection of additional python packages [optional]\n\
+\t-u\t\t\t\tuninstall previous lalsuite install (rather than just doing a 'make distclean') [optional]\n\
+\t-c\t\t\t\tonly re-run 'make install' without 'make distclean' beforehand [optional]\n\
+\t-g\t\t\t\tperform 'git clean -dxf' to remove superfluous files (BE CAREFUL!) [optional]\n\
+\t-p\t\t\t\tpython (the path to the required python executable for installing\n\t\t\t\t\tthe virtual environment) [optional]\n\
+\t-v\t\t\t\tpath to the virtualenvwrapper.sh script [optional]\n\
+\t-o\t\t\t\tcompile lalsuite with the -O3 optimisation [optional]\n\
+\t--disable-doxygen\t\tcompile lalsuite without doxygen documentation (this is enabled by default)\n\
+\t--disable-lal[packagename]\tdisable comilation of a particular LAL package (all enabled by default)\n\
+\t-C\t\t\t\trun 'make check' after installation\n\
+\t-h\t\t\t\thelp\n"
 
 if [[ $# -eq 0 ]]; then
   echo -e $usage
@@ -46,96 +38,191 @@ fi
 nopython=0 # by default install additional python packages
 pythonexe=""
 isbranch=0
-vewscript="jskgkgksbdkuylfzgslf" # some gibberish
+vewscript=""
 optimise=0
 unintsall=0
 gitclean=0
 distclean=1
 withdoc=1
 withcheck=0
+disablepkgs=""
+thisbranch=""
 
-while getopts ":b:p:v:nougcdCh" opt; do
-  case $opt in
-    b)
-      thisbranch=$OPTARG
-      # check argument is one of the current git branches
-      for branch in ${branches[@]}; do
-        if [[ $branch == $thisbranch ]]; then
-          isbranch=1
-          break
-        fi
-      done
+# use getopt to parse command line rather than inbuilt bash getopts (https://stackoverflow.com/a/7948533/1862861)
+TEMP=`getopt -o b:nucgp:v:odCh --longoptions disable-doxygen,disable-lalframe,disable-lalxml,disable-lalmetaio,disable-lalsimulation,disable-lalburst,disable-laldetchar,disable-lalinspiral,disable-lalstochastic,disable-lalpulsar,disable-lalinference -- "$@"`
 
-      if [[ $isbranch -eq 0 ]]; then # check if branch is found
-        echo "Specified branch \"${thisbranch}\" is not in the git repo. Use one of the following:"
-        for branch in ${branches[@]}; do
-          echo "  $branch"
-        done
-        cd $CURDIR
-        exit 0
-      fi
+if [ $? != 0 ]; then
+  echo -e $usage
+  exit 1
+fi
+
+# Note the quotes around `$TEMP': they are essential!
+eval set -- "$TEMP"
+
+echo $TEMP
+
+#while getopts ":b:p:v:nougcdCh" opt; do
+while true; do
+  case "$1" in
+    -b )
+      thisbranch=$2
+      shift 2
       ;;
-    p)
-      pythonexe=$OPTARG
-      if [[ ! -x "$OPTARG" ]]; then
-        echo "$OPTARG: this python executable does not exist or is not executable"
-        cd $CURDIR
-        exit 0
-      fi
-      pythonexe="-p $OPTARG"
+    -p )
+      pythonexe=$2
+      shift 2
       ;;
-    v)
-      vewscript=$OPTARG
-      if [[ ! -x "$vewscript" ]]; then
-      echo "$OPTARG: this virtualenvwrapper.sh script does not exist or is not executable"
-        cd $CURDIR
-        exit 0
-      fi
+    -v )
+      vewscript=$2
+      shift 2
       ;;
-    o)
+    -o )
       optimise=1
+      shift
       ;;
-    n)
+    -n )
       nopython=1
+      shift
       ;;
-    u)
+    -u )
       uninstall=1
+      shift
       ;;
-    c)
+    -c )
       distclean=0 # remove distclean
+      shift
       ;;
-    g)
-      echo "Are you sure you really want to run 'git clean -dxf'? (y/n):"
-      read -n 1 ynanswer
-      if [ "$ynanswer" == "y" ]; then
-        gitclean=1
-      fi
+    -g )
+      gitclean=1
+      shift
       ;;
-    d)
+    --disable-doxygen )
       withdoc=0 # don't compile with doxygen documentation
+      shift
       ;;
-    C)
+    --disable-lalframe )
+      disablepkgs="$disablepkgs --disable-lalframe"
+      shift
+      ;;
+    --disable-lalmetaio )
+      disablepkgs="$disablepkgs --disable-lalmetaio"
+      shift
+      ;;
+    --disable-lalxml )
+      disablepkgs="$disablepkgs --disable-lalxml"
+      shift
+      ;;
+    --disable-lalsimulation )
+      disablepkgs="$disablepkgs --disable-lalsimulation"
+      shift
+      ;;
+    --disable-lalburst )
+      disablepkgs="$disablepkgs --disable-lalburst"
+      shift
+      ;;
+    --disable-laldetchar )
+      disablepkgs="$disablepkgs --disable-laldetchar"
+      shift
+      ;;
+    --disable-lalinspiral )
+      disablepkgs="$disablepkgs --disable-lalinspiral"
+      shift
+      ;;
+    --disable-lalstochastic )
+      disablepkgs="$disablepkgs --disable-lalstochastic"
+      shift
+      ;;
+    --disable-lalpulsar )
+      disablepkgs="$disablepkgs --disable-lalpulsar"
+      shift
+      ;;
+    --disable-lalinference )
+      disablepkgs="$disablepkgs --disable-lalinference"
+      shift
+      ;;
+    --disable-lalapps )
+      disablepkgs="$disablepkgs --disable-lalapps"
+      shift
+      ;;
+    -C )
       withcheck=1 # run 'make check' after installation
+      shift
       ;;
-    h)
+    -h )
       echo -e $usage
       cd $CURDIR
       exit 0
       ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
+    -- )
+      shift
+      break
       ;;
+    * ) break ;;
   esac
 done
 
-if [[ $isbranch -eq 0 ]]; then
+if [[ -z $thisbranch ]]; then
   echo -e "No branch has been given\n"
   echo -e $usage
   cd $CURDIR
   exit 0
 fi
- 
+
+# current location
+CURDIR=`pwd`
+
+# get a list of the current git branch names
+cd $LALSUITE_LOCATION
+
+branches=$(git branch | cut -c 3-) # see http://stackoverflow.com/a/3846451/1862861
+
+# check argument is one of the current git branches
+for branch in ${branches[@]}; do
+  if [[ $branch == $thisbranch ]]; then
+    isbranch=1
+    break
+  fi
+done
+
+if [[ $isbranch -eq 0 ]]; then # check if branch is found
+  echo "Specified branch \"${thisbranch}\" is not in the git repo. Use one of the following:"
+  for branch in ${branches[@]}; do
+    echo "  $branch"
+  done
+  cd $CURDIR
+  exit 0
+fi
+
+# check if a virtualenvwrapper script is given
+if [[ ! -z $vewscript ]]; then
+  if [[ ! -x "$vewscript" ]]; then
+    echo "$2: this virtualenvwrapper.sh script does not exist or is not executable"
+    cd $CURDIR
+    exit 0
+  fi
+fi
+
+# check the python executable
+if [[ ! -z "$pythonexe" ]]; then
+  if [[ ! -x "$pythonexe" ]]; then
+    echo "$2: this python executable does not exist or is not executable"
+    cd $CURDIR
+    exit 0
+  fi
+  pythonexe="-p $pythonexe"
+fi
+
+# check if you really want to do a git clean
+if [[ $gitclean -eq 1 ]]; then
+  echo "Are you sure you really want to run 'git clean -dxf'? (y/n):"
+  read -n 1 ynanswer
+  if [ "$ynanswer" == "y" ]; then
+    gitclean=1
+  else
+    gitclean=0
+  fi
+fi
+  
 baseenv=${HOME}/lscsoft/.virtualenvs
 
 # use virtualenvwrapper
@@ -189,7 +276,6 @@ ENV=$thisbranch
 
 # components of lalsuite
 lalsuite=("lal" "lalframe" "lalmetaio" "lalxml" "lalsimulation" "lalburst" "lalinspiral" "lalpulsar" "lalstochastic" "laldetchar" "lalinference" "lalapps")
-
 lalsuitepy=("glue" "pylal")
 
 # check if virtual environment already exists - if not create it otherwise activate it
@@ -277,43 +363,14 @@ if [[ $withdoc -eq 1 ]]; then
   edoxygen="--enable-doxygen" # set doxygen flag
 fi
   
-# set the flags and prefix for each part of lalsuite
-declare -A lalsuiteflags=(["lal"]="$eswig $edoxygen" \
-                          ["lalframe"]="$eswig $edoxygen" \
-                          ["lalmetaio"]="$eswig $edoxygen" \
-                          ["lalxml"]="$eswig $edoxygen" \
-                          ["lalsimulation"]="$eswig $edoxygen" \
-                          ["lalburst"]="$eswig $edoxygen" \
-                          ["lalinspiral"]="$eswig $edoxygen" \
-                          ["lalpulsar"]="$eswig --enable-lalxml $edoxygen" \
-                          ["lalstochastic"]="$eswig $edoxygen" \
-                          ["laldetchar"]="$eswig $edoxygen" \
-                          ["lalinference"]="$eswig $eopenmp $edoxygen" \
-                          ["lalapps"]="--enable-lalxml $empi $eopenmp $edoxygen" \
-                          ["glue"]="" \
-                          ["pylal"]="")
-
 LSCSOFT_PREFIX=$baseenv/$ENV
-
-declare -A lalsuiteprefixes=(["lal"]="$LSCSOFT_PREFIX" \
-                             ["lalframe"]="$LSCSOFT_PREFIX" \
-                             ["lalmetaio"]="$LSCSOFT_PREFIX" \
-                             ["lalxml"]="$LSCSOFT_PREFIX" \
-                             ["lalsimulation"]="$LSCSOFT_PREFIX" \
-                             ["lalburst"]="$LSCSOFT_PREFIX" \
-                             ["lalinspiral"]="$LSCSOFT_PREFIX" \
-                             ["lalpulsar"]="$LSCSOFT_PREFIX" \
-                             ["lalstochastic"]="$LSCSOFT_PREFIX" \
-                             ["laldetchar"]="$LSCSOFT_PREFIX" \
-                             ["lalinference"]="$LSCSOFT_PREFIX" \
-                             ["lalapps"]="$LSCSOFT_PREFIX" \
-                             ["glue"]="$LSCSOFT_PREFIX" \
-                             ["pylal"]="$LSCSOFT_PREFIX")
 
 extracflags=""
 if [[ $optimise -eq 1 ]]; then
   extracflags=-O3
 fi
+
+enableflags="--enable-mpi --enable-cfitsio"
 
 # perform git clean to remove old/superfluous files and directories from the repository
 if [[ $gitclean -eq 1 ]]; then
@@ -321,48 +378,38 @@ if [[ $gitclean -eq 1 ]]; then
 fi
 
 # install components of lalsuite
-for lalc in ${lalsuite[@]}; do
-  cd $lalc
+if [[ $uninstall -eq 1 ]]; then
+  make uninstall
+fi
 
-  if [[ $uninstall -eq 1 ]]; then
-    make uninstall
-  fi
+if [[ $distclean -eq 1 ]]; then
+  make distclean
+  ./00boot
+  ./configure --prefix=$LSCSOFT_PREFIX $enableflags CFLAGS=$extracflags
+fi
 
-  if [[ $distclean -eq 1 ]]; then
-    make distclean
-    ./00boot
-    ./configure --prefix=${lalsuiteprefixes["$lalc"]} ${lalsuiteflags["$lalc"]} CFLAGS=$extracflags
-  fi
+# make and install
+make install -j4
 
-  # make and install
-  make install -j4
+# make and install documentation
+if [[ $withdoc -eq 1 ]]; then
+  make install-html -j4
+fi
 
-  # make and install documentation
-  if [[ $withdoc -eq 1 ]]; then
-    make install-html -j4
-  fi
-
-  if [[ $withcheck -eq 1 ]]; then
-    make check
-  fi
-
-  # source config scripts
-  if [ -f ${lalsuiteprefixes["$lalc"]}/etc/${lalc}-user-env.sh ]; then
-    . ${lalsuiteprefixes["$lalc"]}/etc/${lalc}-user-env.sh
-  fi
-  cd ..
-done
+if [[ $withcheck -eq 1 ]]; then
+  make check
+fi
 
 # install python-based components of lalsuite
 for lalc in ${lalsuitepy[@]}; do
   cd $lalc
   # remove stuff in build directory
   rm -rf $lalc/build
-  python setup.py install --prefix=${lalsuiteprefixes["$lalc"]}
+  python setup.py install --prefix=$LSCSOFT_PREFIX
 
   # source config scripts
-  if [ -f ${lalsuiteprefixes["$lalc"]}/etc/${lalc}-user-env.sh ]; then
-    . ${lalsuiteprefixes["$lalc"]}/etc/${lalc}-user-env.sh
+  if [ -f $LSCSOFT_PREFIX/etc/${lalc}-user-env.sh ]; then
+    . $LSCSOFT_PREFIX/etc/${lalc}-user-env.sh
   fi
   cd ..
 done
